3533ec19f7950835f96581c214a40fcaeb275d98
SHSR2001 <shyams.aat@gmail.com>
2020-12-30 11:25:37 +0800

    Update table.js

--- js/table.js
+++ js/table.js
@@ -44,7 +44,7 @@ function appendEvent( event ) {
   //var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
   console.log(event.tickets);
   
-  if (event.tickets.includes("events.html#sutd") || event.location.includes("events.html#Online") {
+  if (event.location.includes("events.html#Online") {
     var eventElement   = $('<div class="event" style="background-color: #8abfed;"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
   else {
     var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
