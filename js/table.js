1e26e79f2ffbb21c0b72049511e157977ec561c5
SHSR2001 <shyams.aat@gmail.com>
2020-12-30 11:43:21 +0800

    Update table.js

--- js/table.js
+++ js/table.js
@@ -42,13 +42,6 @@ function appendEvent( event ) {
   var eventStartDate = new Date(event.startdate)
   var eventEndDate   = new Date(event.enddate)
   var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
-  console.log(event.tickets);
-  
-  //if (event.location.includes("Online")) {
-    //var eventElement   = $('<div class="event" style="background-color: #8abfed;"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
-  //else {
-    //var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
-  //}
 
   // Handle multi-days
   if ( eventEndDate.getDate() ) {
